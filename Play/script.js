bfb98d3c320166decb33356ff2245ee189d22137
Ondřej Honus <honusondrej@gmail.com>
2024-01-06 17:33:48 +0100

    04.1 - red balls omg

--- Play/script.js
+++ Play/script.js
@@ -1,5 +1,5 @@
 let player;
-
+let blinkingBalls = [];
 
 //**************************************************************//
 //************************PLAYER**************************//
@@ -13,17 +13,20 @@ class Player {
         this.h = 40;
         this.v = 10;
     }
+
+// MOVEMENT SYSTEM
+
     move() {
-        if (keyIsDown(87)) {
+        if (keyIsDown(87) || keyIsDown(UP_ARROW)) {
             if (this.y > this.h / 2) this.y -= this.v;
         }
-        if (keyIsDown(83)) {
+        if (keyIsDown(83) || keyIsDown(DOWN_ARROW)) {
             if (this.y < height - this.h / 2) this.y += this.v;
         }
-        if (keyIsDown(65)) {
+        if (keyIsDown(65) || keyIsDown(LEFT_ARROW)) {
             if (this.x > this.w / 2) this.x -= this.v;
         }
-        if (keyIsDown(68)) {
+        if (keyIsDown(68) || keyIsDown(RIGHT_ARROW)) {
             if (this.x < width - this.w / 2) this.x += this.v;
         }
     }
@@ -39,17 +42,48 @@ class Player {
     }
 
 //**************************************************************//
+//************************BALL**************************//
+//**************************************************************//
+
+class BlinkingBall{
+    constructor() {
+        this.x = random(0, width);
+        this.y = random(0, height);
+        this.w = 40;
+        this.h = 40;
+        }
+    draw() {
+        fill('red');
+        rectMode(CENTER);
+        push();
+        translate(this.x, this.y);
+        rect(0, 0, this.w, this.h, 360, 360);
+        pop();
+      }
+}
+
+//**************************************************************//
 //************************SETUP / DRAW**************************//
 //**************************************************************//
 
     function setup() {
         canvas = createCanvas(window.innerWidth-75, window.innerHeight-75);
         player = new Player(width / 2, height / 2);
+  //    blinkingBalls = new BlinkingBall(Math.floor(Math.random() * (width-75)), Math.floor(Math.random() * (height-75)));
+
       }
       function draw() {
-        background(0);
+        background('#000216');
         player.draw();
+
+        if (frameCount % 60 == 0) {
+            blinkingBalls.push(new BlinkingBall());
+            console.log("new ball")
     }
+        blinkingBalls.forEach((blinkingBall, idx, arr) => {
+            blinkingBall.draw(); 
+        });
+}
 
 //**************************************************************//
 //************************LEVEL SYSTEM**************************//
@@ -62,10 +96,10 @@ function saveTopLevel(level) {
 
 function getTopLevel() {
     const savedLevel = localStorage.getItem('topLevel');
-    return savedLevel ? parseInt(savedLevel, 10) : 1;
+    return savedLevel ? parseInt(savedLevel, 10) : 0;
 }
 
-document.addEventListener('keydown', function (event) {
+/*document.addEventListener('keydown', function (event) {
     if (event.code === 'Space') {
         let currentLevel = parseInt(document.getElementById('currentLevelDisplay').textContent.split(":")[1].trim(), 10) || 0;
         let topLevel = getTopLevel();
@@ -80,6 +114,7 @@ document.addEventListener('keydown', function (event) {
         updateLevelDisplay('current', currentLevel);
     }
 });
+*/
 function updateLevelDisplay(type, level) {
     const displayElement = document.getElementById(type + 'LevelDisplay');
     displayElement.textContent = (type === 'current' ? 'Level: ' : 'Top Level: ') + level;
