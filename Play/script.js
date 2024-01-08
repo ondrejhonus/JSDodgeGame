88506758615227bc8d25e61c75a2cc04d6672145
Ondřej Honus <honusondrej@gmail.com>
2024-01-08 19:41:29 +0100

    07 - Seconds counter and harder game

--- Play/script.js
+++ Play/script.js
@@ -1,7 +1,22 @@
 let player;
 let blinkingBalls = [];
 let hit = false;
+let seconds = 0;
+let topSecondsLived = localStorage.getItem('topSecondsLived') || 0;
 
+
+
+
+// Making cheating impossible ig idk who knows
+
+document.addEventListener('visibilitychange', handleVisibilityChange);
+function handleVisibilityChange() {
+    if (document.hidden) {
+        clearInterval(timerInterval);
+    } else {
+        timerInterval = setInterval(updateCounter, 1000);
+    }
+}
 //**************************************************************//
 //************************PLAYER**************************//
 //**************************************************************//
@@ -16,7 +31,7 @@ class Player {
         this.color = 'f'
     }
 
-// MOVEMENT SYSTEM
+    // MOVEMENT SYSTEM
 
     move() {
         if (keyIsDown(87) || keyIsDown(UP_ARROW)) {
@@ -40,26 +55,26 @@ class Player {
         translate(this.x, this.y);
         rect(0, 0, this.w, this.h, 360, 360);
         pop();
-      }
     }
+}
 
 //**************************************************************//
 //************************BALL**************************//
 //**************************************************************//
 
 class BlinkingBall {
-    constructor(x = random(0, width), y = random(0, height), dirX = 0, dirY = 0, w = 40, h = 40, parent = true) {
+    constructor(x = random(0, width), y = random(0, height), dirX = 0, dirY = 0, w = 40, h = 40, parent = true, appear = 1500) {
         this.x = x;
         this.y = y;
         this.w = w;
         this.h = h;
         this.dirX = dirX;
         this.dirY = dirY;
-        this.creationTime = millis(); 
-        this.appearDuration = 2000;
-        this.explosionDuration = 500;
+        this.creationTime = millis();
+        this.appearDuration = appear;
+        this.explosionDuration = 200;
         this.explosions = [];
-        this.parent = parent; 
+        this.parent = parent;
 
         if (this.parent) {
             blinkingBalls.push(this);
@@ -73,6 +88,10 @@ class BlinkingBall {
 
     draw() {
         let elapsedTime = millis() - this.creationTime;
+        let smallBalls = 10;
+        if (frameCount % 60 == 0) {
+            smallBalls + 10;
+        }    
 
         if (this.drawn()) {
             // Red ball appearance
@@ -87,17 +106,19 @@ class BlinkingBall {
         } else if (this.parent && elapsedTime < this.appearDuration + this.explosionDuration) {
             let newSize = map(elapsedTime - this.appearDuration, 0, this.explosionDuration, this.w, 50);
             let newColor = lerpColor(color('red'), color(255), (elapsedTime - this.appearDuration) / this.explosionDuration);
+
             if (this.explosions.length < 10) {
-                for (let index = 0; index < 10; index++) {
-                    let angle = Math.random() * Math.PI * 2; // Random angle in radians
-                    let distance = Math.random() * 2 + 2; // Random distance between 50 and 100
+
+                for (let index = 0; index < smallBalls; index++) {
+                    let angle = Math.random() * Math.PI * 2;
+                    let distance = Math.random() * 2 + 2;
                     let x = Math.cos(angle) * distance;
                     let y = Math.sin(angle) * distance;
-                if (random(-1, 1) > 0) x *= -1;
-                if (random(-1, 1) > 0) y *= -1;
-                this.explosions.push(new BlinkingBall(this.x, this.y, x, y , 10, 10, false));
+                    if (random(-1, 1) > 0) x *= -1;
+                    if (random(-1, 1) > 0) y *= -1;
+                    this.explosions.push(new BlinkingBall(this.x, this.y, x, y, 10, 10, false, 5000));
+                }
             }
-        }
 
             fill(newColor);
             rectMode(CENTER);
@@ -106,7 +127,7 @@ class BlinkingBall {
             rect(0, 0, newSize, newSize, 360, 360);
             pop();
         } else {
-            // The ball has disappeared
+            // Remove ball from keš
             if (!this.explosions) return;
             for (const explosion of this.explosions) {
                 if (explosion.parent) continue;
@@ -115,7 +136,7 @@ class BlinkingBall {
             for (let index = 0; index < this.explosions.length; index++) {
                 const element = this.explosions[index];
                 if (!element.drawn()) this.explosions.splice(index, 1);
-            } 
+            }
             return;
         }
     }
@@ -125,38 +146,50 @@ class BlinkingBall {
 //************************SETUP / DRAW**************************//
 //**************************************************************//
 
-    function setup() {
-        canvas = createCanvas(window.innerWidth-75, window.innerHeight-75);
-        player = new Player(width / 2, height / 2);
-  //    blinkingBalls = new BlinkingBall(Math.floor(Math.random() * (width-75)), Math.floor(Math.random() * (height-75)));
+function setup() {
+    canvas = createCanvas(window.innerWidth - 75, window.innerHeight - 75);
+    player = new Player(width / 2, height / 2);
+    //    blinkingBalls = new BlinkingBall(Math.floor(Math.random() * (width-75)), Math.floor(Math.random() * (height-75)));
+    frameRate(60);
 
-      }
-      function draw() {
-        background('#222222');
-        player.draw();
+}
+function draw() {
+    background('rgba(34, 34, 34, 1)');
+    player.draw();
+    document.getElementById("topSecondsLived").innerText = 'Best run: ' + topSecondsLived + 's';
 
-        if (frameCount % 60 == 0) {
-            blinkingBalls.push(new BlinkingBall());
-            console.log("new ball")
+    if (frameCount % 60 == 0) {
+        blinkingBalls.push(new BlinkingBall());
+        console.log("new ball");
     }
-       /* blinkingBalls.forEach((blinkingBall) => {
-            blinkingBall.draw();
-        });
-        */
-        
+    if (frameCount % 60 == 0) {
+        seconds++;
+        document.getElementById("currentSeconds").innerText = seconds + 's';
 
-        blinkingBalls.forEach(function (ball, idx, arr) {
-            ball.draw();
-            if(!ball.explosions) return;
-            for (const explosion of ball.explosions) {
-                hit = collideCircleCircle(player.x, player.y, player.w, explosion.x, explosion.y, explosion.w);
-                if(hit){
-                    window.location.replace("../TryAgain/");
-                }
+        if (seconds > topSecondsLived) {
+            topSecondsLived = seconds;
+            localStorage.setItem('topSecondsLived', topSecondsLived);
+        }
+    }
+    /* blinkingBalls.forEach((blinkingBall) => {
+         blinkingBall.draw();
+     });
+     */
+
+
+    blinkingBalls.forEach(function (ball, idx, arr) {
+        ball.draw();
+        if (!ball.explosions) return;
+        for (const explosion of ball.explosions) {
+            hit = collideCircleCircle(player.x, player.y, player.w, explosion.x, explosion.y, explosion.w);
+            if (hit) {
+                //window.location.replace("../TryAgain/");
             }
-        
+        }
 
-        });
+
+    });
+
 }
 
 //**************************************************************//
@@ -164,34 +197,5 @@ class BlinkingBall {
 //**************************************************************//
 
 
-function saveTopLevel(level) {
-    localStorage.setItem('topLevel', level.toString());
-}
 
-function getTopLevel() {
-    const savedLevel = localStorage.getItem('topLevel');
-    return savedLevel ? parseInt(savedLevel, 10) : 0;
-}
 
-/*document.addEventListener('keydown', function (event) {
-    if (event.code === 'Space') {
-        let currentLevel = parseInt(document.getElementById('currentLevelDisplay').textContent.split(":")[1].trim(), 10) || 0;
-        let topLevel = getTopLevel();
-
-        currentLevel++;
-
-        if (currentLevel > topLevel) {
-            topLevel = currentLevel;
-            saveTopLevel(topLevel);
-            updateLevelDisplay('top', topLevel);
-        }
-        updateLevelDisplay('current', currentLevel);
-    }
-});
-*/
-function updateLevelDisplay(type, level) {
-    const displayElement = document.getElementById(type + 'LevelDisplay');
-    displayElement.textContent = (type === 'current' ? 'Level: ' : 'Top Level: ') + level;
-}
-const initialTopLevel = getTopLevel();
-updateLevelDisplay('top', initialTopLevel);
